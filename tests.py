e3c30e41bfcabf3dd89e85fb6df5bf33a50fb467
Peter Rose <pwrose@ucsd.edu>
2018-11-10 00:29:01 -0800

    set timeout to -1 = no time limit

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ def exec_notebook(path):
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
         print(fout.name)
         args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
-#                "--ExecutePreprocessor.timeout=100000",
+                "--ExecutePreprocessor.timeout=-1",
                 "--output", fout.name, path]
         subprocess.check_call(args)
 
